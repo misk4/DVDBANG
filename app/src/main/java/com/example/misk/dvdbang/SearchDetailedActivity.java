f725c4618b104b41c1cb9d29346e673be2e7f78d
misk4 <misk4@naver.com>
2015-12-01 20:00:42 +0900

    3rd

--- app/src/main/java/com/example/misk/dvdbang/SearchDetailedActivity.java
+++ app/src/main/java/com/example/misk/dvdbang/SearchDetailedActivity.java
@@ -4,13 +4,60 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SearchDetailedActivity extends Activity {
+public class SearchDetailedActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+    Button detailedSearchButton;
+    Spinner spinnerGenre,spinnerYear,spinnerTime;
+    ArrayAdapter adapterGenre,adapterYear,adapterTime;
+    EditText keywordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_detailed);
+
+        keywordInput=(EditText)findViewById(R.id.keywordInput);
+        detailedSearchButton=(Button)findViewById(R.id.detailedSearchButton);
+        detailedSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        spinnerGenre=(Spinner)findViewById(R.id.genreSpinner);
+        spinnerTime=(Spinner)findViewById(R.id.movieTimeSpinner);
+        spinnerYear=(Spinner)findViewById(R.id.movieYearSpinner);
+
+        adapterGenre = ArrayAdapter.createFromResource(
+                this, R.array.genre,android.R.layout.simple_spinner_item);
+        adapterGenre.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGenre.setAdapter(adapterGenre);
+        spinnerGenre.setSelection(1);
+        spinnerGenre.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3){
+        switch(arg0.getId()){
+            case R.id.spinner:
+                genre = (String) arg0.getSelectedItem();
+        }
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0){
+        Toast.makeText(this, "하나를 선택해 주십시오", Toast.LENGTH_SHORT).show();
     }
 
     @Override
